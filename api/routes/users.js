3f2b14c6f69dee38a9caf5b3895f3444f0d47d62
Anmol Gupta <anmolgupta@Anmols-MacBook-Air.local>
2023-06-04 00:59:16 +0530

    JWT verification setup

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,7 +1,31 @@
 import express  from "express";
+import { updateUser,deleteUser,getUser,getUsers } from "../controllers/user.js";
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
+
 const router=express.Router();
 
+router.get("/checkAuthentication",verifyToken,(req,res,next)=>{
+    res.send("Hello user,you are logged in!")
+});
 
+router.get("/checkUser/:id", verifyUser,(req,res,next)=>{
+    res.send("Hello user,you are logged in and can delete your account!")
+});
 
+router.get("/checkadmin/:id", verifyAdmin,(req,res,next)=>{
+    res.send("Hello Admin,you are logged in and can delete all accounts!")
+});
 
-export default router;
+//update
+router.put("/:id",updateUser);
+
+//delete
+router.delete("/:id",deleteUser);
+
+//get
+router.get("/:id",getUser);
+
+//getall
+router.get("/",getUsers);
+
+export default router; 
