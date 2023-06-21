9b00dcd19a7506dff51574d1705e03df8c354e94
Anmol Gupta <anmolgupta@Anmols-MacBook-Air.local>
2023-06-22 03:34:42 +0530

    Adding reactjs

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,5 +1,5 @@
 import express  from "express";
-import { createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotel.js";
+import { countByCity, createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotel.js";
 import Hotel from "../models/Hotel.js";
 import {verifyAdmin} from "../utils/verifyToken.js"
 
@@ -13,10 +13,12 @@ router.put("/:id",verifyAdmin,updateHotel);
 router.delete("/:id",deleteHotel);
 
 //get
-router.get("/:id",getHotel);
+router.get("/find/:id",getHotel);
 
 //getall
 router.get("/",getHotels);
+router.get("/countByCity",countByCity);
+router.get("/countByType",getHotels);
 
 
 
