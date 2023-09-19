4e51d57d794cdd4fdcb901f469eee05727ebe4c8
imAnmolGupta <anmolgupta682@gmail.com>
2023-09-20 02:28:30 +0530

    implemented search using api

--- client/src/components/featured/Featured.jsx
+++ client/src/components/featured/Featured.jsx
@@ -2,10 +2,10 @@ import useFetch from "../../hooks/useFetch.js";
 import "./featured.css";
 
 const Featured = () => {
-  const {data,loading,error}= useFetch("/hotels/countByCity?cities=kolkata,Agra,mumbai,nepal");
+  const {data,loading,error}= useFetch("/hotels/countByCity?cities=kolkata,agra,mumbai");
   return (
     <div className="featured">
-      {loading? ("loading please wait") :( <> <div className="featuredItem">
+      {loading? ("loading please wait") : ( <> <div className="featuredItem">
         <img
           src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
           alt=""
