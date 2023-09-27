593ffc44d2f8fb55b505943b3f649f32a90f6c6a
imAnmolGupta <anmolgupta682@gmail.com>
2023-09-28 04:18:29 +0530

    rooms api configure

--- client/src/pages/hotel/Hotel.jsx
+++ client/src/pages/hotel/Hotel.jsx
@@ -12,8 +12,10 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { useContext, useState } from "react";
 import useFetch from "../../hooks/useFetch.js"
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { SearchContext } from '../../context/SearchContext';
+import { AuthContext } from "../../context/AuthContext";
+import Reserve from "../../components/reserve/Reserve";
 
 const Hotel = () => {
 
@@ -21,8 +23,12 @@ const Hotel = () => {
   const id = location.pathname.split("/")[2];
   const [slideNumber, setSlideNumber] = useState(0);
   const [open, setOpen] = useState(false);
+  //for if user login condition
+  const [openModal, setOpenModal] = useState(false);
 
   const { data, loading, error} = useFetch(`/hotels/find/${id}`);
+  const { user} = useContext(AuthContext);
+  const navigate=useNavigate()
 
   const {dates,options}=useContext(SearchContext);
 
@@ -53,6 +59,14 @@ const Hotel = () => {
     setSlideNumber(newSlideNumber)
   };
 
+
+  const handleClick=() =>{
+    if(user){
+      setOpenModal(true);
+    }else{
+      navigate("/login")
+    }
+  }
   return (
     <div>
       <Navbar />
@@ -122,13 +136,14 @@ const Hotel = () => {
               <h2>
                 <b>${ days  * data.cheapestPrice* options.room}</b> ({days} nights)
               </h2>
-              <button>Reserve or Book Now!</button>
+              <button onClick={handleClick}>Reserve or Book Now!</button>
             </div>
           </div>
         </div>
         <MailList />
         <Footer />
       </div>)}
+      {openModal &&  <Reserve setOpen={setOpenModal} hotelId={id} />}
     </div>
   );
 };
